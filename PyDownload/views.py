ac362d202c7792a0c35bd3a238ce3f6b9cbc5bf4
khannakshat7 <khannakshat7@gmail.com>
2019-11-30 00:16:20 +0530

    Set Up

--- PyDownload/views.py
+++ PyDownload/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    mydict = {'content':"Hello!! Demo text"}
+    return render(request,'index.html',context=mydict)
